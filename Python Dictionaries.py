ee4a691cf47059813ee4fdc46d3270fcace91112
wcaughey-tt <wcaughey@thumbtack.com>
2020-08-12 08:57:04 -0600

    Learning Python

---
+++ Python Dictionaries.py
@@ -0,0 +1,9 @@
+
+
+friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+print(friend_ages["Rolf"])
+
+# So, my dictionary has keys (the names)
+# Each name has a 'definition' (age)
+
+friend_ages["Bob"] = 20
